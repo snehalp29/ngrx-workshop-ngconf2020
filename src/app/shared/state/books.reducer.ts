d3853aab53b9587df879949cb2d7d325e7f96de8
snehalp29 <snehalpatkar@Gmail.com>
2020-07-12 19:29:45 -0400

    added reducre

--- src/app/shared/state/books.reducer.ts
+++ src/app/shared/state/books.reducer.ts
@@ -1,3 +1,83 @@
 import { createReducer, on, Action, createSelector } from "@ngrx/store";
 import { BookModel, calculateBooksGrossEarnings } from "src/app/shared/models";
 import { BooksPageActions, BooksApiActions } from "src/app/books/actions";
+
+const createBook = (books: BookModel[], book: BookModel) => [...books, book];
+const updateBook = (books: BookModel[], changes: BookModel) =>
+    books.map((book) => {
+        return book.id === changes.id ? Object.assign({}, book, changes) : book;
+    });
+const deleteBook = (books: BookModel[], bookId: string) =>
+    books.filter((book) => bookId !== book.id);
+
+export interface State {
+    collection: BookModel[];
+    activeBookId: string | null;
+}
+
+export const initialState: State = {
+    collection: [],
+    activeBookId: null,
+};
+
+export const booksReducer = createReducer(
+    initialState,
+    on(
+        BooksPageActions.enter,
+        BooksPageActions.clearSelectedBook,
+        (state, action) => {
+            return {
+                ...state,
+                activeBookId: null,
+            };
+        }
+    ),
+    on(BooksPageActions.selectBook, (state, action) => {
+        return {
+            ...state,
+            activeBookId: action.bookId,
+        };
+    }),
+    on(BooksApiActions.booksLoaded, (state, action) => {
+        return {
+            ...state,
+            collection: action.books
+        }
+    }),
+    on(BooksApiActions.booksCreated, (state, action) => {
+        return {
+            ...state,
+            collection: createBook(state.collection, action.book)
+        }
+    }),
+    on(BooksApiActions.bookUpdated, (state, action) => {
+        return {
+            ...state,
+            collection: updateBook(state.collection, action.book)
+        }
+    }),
+    on(BooksApiActions.bookDeleted, (state, action) => {
+        return {
+            ...state,
+            collection: deleteBook(state.collection, action.bookId)
+        }
+    })
+);
+
+// AOT Compilation
+export function reducer(state: undefined | State, action: Action) {
+    return booksReducer(state, action);
+}
+// Getter Selectors
+export const selectAll = (state: State) => state.collection;
+export const selectActiveBookId = (state: State) => state.activeBookId;
+// Complex Selector
+export const selectActiveBook = createSelector(
+    selectAll,
+    selectActiveBookId,
+    (books, activeBookId) => books.find(book => book.id === activeBookId) || null
+);
+export const selectEarningsTotals = createSelector(
+    selectAll,
+    calculateBooksGrossEarnings
+);
